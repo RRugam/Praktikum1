ebfb1394ecf5f0d7e1d9b83559a4789d3a41f5ba
RRugam <rugam.rasmus@gmail.com>
2019-02-10 19:04:49 +0200

    Spring boot + Vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Brand List</h1>
+    <div v-for="brand in brands">
+      {{ brand.name }}
+    </div>
   </div>
 </template>
+
+
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import axios from 'axios';
+
+
+export interface Brand {
+  id: number;
+  name: string;
+  giphyUrl: string;
+}
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
-</script>
+export default class Home extends Vue {
+  public brands: Brand[] = [];
+
+  private async created() {
+    const response = await axios.get('/good-brands');
+    this.brands = await response.data;
+  }
+}
+
+
